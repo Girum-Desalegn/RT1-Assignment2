beac32b2e62d5660933e77c6fc1a1984fc431f88
Giurm-Desalegn <girumolla@gmail.com>
2024-01-14 18:47:22 +0000

    uploaded

---
+++ scripts/service_return_Node_B.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Import Libraries
+import rospy
+import actionlib
+import actionlib.msg
+import assignment_2_2023.msg
+from std_srvs.srv import *
+import sys
+import select
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Twist
+from assignment_2_2023.msg import Vel_pos
+from colorama import Fore, Style
+from colorama import init
+init()
